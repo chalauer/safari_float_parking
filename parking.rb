5e664d4a547dcd75870d676cd5ea59b9abc89b37
Audrey Lauer <chalauer@gmail.com>
2025-01-18 21:22:12 +0000

    tweaks

--- parking.rb
+++ parking.rb
@@ -12,39 +12,31 @@ class Parking
       if @float.empty?
         puts "No floats parked. Would you like to 'park' a float or 'exit' to leave?"
       else
-        puts "Type 'park', 'retrieve', or 'list' or 'exit' to leave."
+        puts "Type 'park', 'retrieve', 'list', or 'exit' to leave."
       end
       
       reason_for_visit = gets.chomp
 
       case reason_for_visit
-
       when "park"
-        log()
-        drive_in(@animal, @vehicle, @color, @decoration)
-
+        animal, vehicle, color, decoration = log()
+        drive_in(animal, vehicle, color, decoration)
       when "retrieve"
-        log()
-        drive_out(@animal, @vehicle, @color, @decoration)
-        
+        animal, vehicle, color, decoration = log()
+        drive_out(animal, vehicle, color, decoration)
       when "list"
         options()
-        
       when "exit"
         puts "See you next time at Safari Float Parking!"
         break
-        
       else
         puts "It seems you may have made a typo. Try again."
       end
     end
   end
-  
-  
-
 
   private
-  
+
   def drive_in(animal, vehicle, color, decoration)
     float = FloatV.new(animal, vehicle, color, decoration)
     @float.push(float)
@@ -64,24 +56,25 @@ class Parking
     end
   end
   
-  def log()
-    puts "enter animal"
-    @animal = gets.chomp
-    puts "enter vehicle"
-    @vehicle = gets.chomp
-    puts "enter color"
-    @color = gets.chomp
-    puts "enter decoration"
-    @decoration = gets.chomp
+  def log
+    puts "Enter animal:"
+    animal = gets.chomp
+    puts "Enter vehicle:"
+    vehicle = gets.chomp
+    puts "Enter color:"
+    color = gets.chomp
+    puts "Enter decoration:"
+    decoration = gets.chomp
+    return animal, vehicle, color, decoration
   end
   
-  def options()
+  def options
     if @float.empty?
-      "No floats are parked here at the moment."
+      puts "No floats are parked here at the moment."
     else
       @float.each do |float|
         puts float.to_s
       end
     end
   end
-end          
+end
